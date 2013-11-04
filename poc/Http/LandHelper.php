347bcc9bf07c2baaef4394d186536a843a713acc
tomponline <thomas.parrott@infinity-tracking.com>
2013-11-04 12:32:46 +0000

    adds poc

--- poc/Http/LandHelper.php
+++ poc/Http/LandHelper.php
@@ -13,5 +13,22 @@ use Infinity\Common\Http\LandHelper;
 $autoloader = new Autoloader();
 $autoloader->register();
 
+
+$_SERVER['HTTP_HOST']       = 'www.mysite.com';
+$_SERVER['HTTP_REFERER']    =
+    'http://www.othersite.com/page1.html?someparam=somevalue';
+$_GET['myTag']  = 'hello world';
+$_GET['tag2']   = 'hello world 2';
+
 $landHelper = new LandHelper();
 
+$persistTags = array(
+    'myTag' => 'session',
+    'tag2'  => 'P2D',
+);
+
+$landHelper->setPersistedLandParamRules($persistTags);
+
+$landHelper->run();
+
+var_dump($_COOKIE);
