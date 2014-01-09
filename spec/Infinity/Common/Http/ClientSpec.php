0827b85681089d3d9910a2090cb51ecb5c619c11
George Arscott <george.arscott@infinitycloud.com>
2014-01-09 15:19:55 +0000

    Adds HTTP client, poc, tests and docs

---
+++ spec/Infinity/Common/Http/ClientSpec.php
@@ -0,0 +1,221 @@
+<?php
+
+namespace spec\Infinity\Common\Http;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class ClientSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType( 'Infinity\Common\Http\Client' );
+    }
+
+    function it_has_a_getChecksMode_method()
+    {
+        $this->getChecksMode()->shouldReturn( TRUE );
+    }
+
+    function it_has_a_getProxy_method()
+    {
+        $this->getProxy()->shouldReturn( FALSE );
+    }
+
+    function it_has_a_getProxyPort_method()
+    {
+        $this->getProxyPort()->shouldReturn( FALSE );
+    }
+
+    function it_has_a_getResponseHeader_method()
+    {
+        $this->setRequestUri( 'localhost' );
+        $this->sendRequest()->shouldReturn( TRUE );
+        $this->getResponseHeader( 'User-Agent' )->shouldReturn( FALSE );
+    }
+
+    function it_has_a_getResponseCode_method()
+    {
+        $this->setRequestUri( 'localhost' );
+        $this->sendRequest()->shouldReturn( TRUE );
+        $this->getResponseCode()->shouldReturn( 200 );
+    }
+
+    function it_has_a_getResponseHandle_method()
+    {
+        $this->getResponseHandle()->shouldReturn( FALSE );
+    }
+
+    function it_has_a_getResponseSize_method()
+    {
+        $this->setRequestUri( 'localhost' );
+        $this->sendRequest()->shouldReturn( TRUE );
+        $this->getResponseSize()->shouldReturn( 0 );
+    }
+
+    function it_has_a_getDownloadedSize_method()
+    {
+        $this->setRequestUri( 'localhost' );
+        $this->sendRequest()->shouldReturn( TRUE );
+        $this->getResponseSize()->shouldReturn( 0 );
+    }
+
+    function it_has_a_getResponseResult_method()
+    {
+        $this->setRequestUri( 'localhost' );
+        $this->sendRequest()->shouldReturn( TRUE );
+        $this->getResponseResult()->shouldReturn( TRUE );
+    }
+
+    function it_has_a_getResponseError_method()
+    {
+        $this->getResponseError()->shouldReturn( FALSE );
+    }
+
+    function it_has_a_getError_method()
+    {
+        $this->getError()->shouldReturn( FALSE );
+    }
+
+    function it_has_a_getCurlHandle_method()
+    {
+        $this->getCurlHandle()->shouldReturn( NULL );
+    }
+
+    function it_has_a_getRequestUri_method()
+    {
+        $this->getRequestUri()->shouldReturn( FALSE );
+    }
+
+    function it_has_a_sendRequest_method()
+    {
+        $this->sendRequest()->shouldReturn( FALSE );
+    }
+
+    //Set checks
+    function it_has_a_setTimeout_method()
+    {
+        $this->setTimeout( 30 )->shouldReturn( NULL );
+    }
+
+    function it_has_a_setRequestTimeout_method()
+    {
+        $this->setRequestTimeout( 30 )->shouldReturn( NULL );
+    }
+
+    function it_has_a_setConnectTimeout_method()
+    {
+        $this->setConnectTimeout( 30 )->shouldReturn( NULL );
+    }
+
+    function it_has_a_setRequestUri_method()
+    {
+        $this->setRequestUri( 'localhost' )->shouldReturn( NULL );
+        $this->getRequestUri()->shouldReturn( 'localhost' );
+    }
+
+    function it_has_a_setChecksMode_method()
+    {
+        $this->setChecksMode( FALSE )->shouldReturn( NULL );
+        $this->getChecksMode()->shouldReturn( FALSE );
+    }
+
+    function it_has_a_setFollowRedirects_method()
+    {
+        $this->setFollowRedirects( FALSE )->shouldReturn( NULL );
+    }
+
+    function it_has_a_setRequestHeader_method()
+    {
+        $this->setRequestHeader( 'User-Agent', 'test' )->shouldReturn( NULL );
+    }
+
+    function it_has_a_setRequestCredentials_method()
+    {
+        $this->setRequestCredentials( 'user', 'passwd' )->shouldReturn( NULL );
+    }
+
+    function it_has_a_setRequestMethod_method()
+    {
+        $this->setRequestMethod( 'POST' )->shouldReturn( NULL );
+    }
+
+    function it_has_a_setRequestData_method()
+    {
+        $data = array(
+            'foo'   => 'bar',
+        );
+
+        $this->setRequestData( $data )->shouldReturn( NULL );
+    }
+
+    function it_has_a_setRequestPutData_method()
+    {
+        $data = array(
+            'foo'   => 'bar',
+        );
+
+        $this->setRequestPutData( $data )->shouldReturn( NULL );
+    }
+
+    function it_has_a_setRequestDeleteData_method()
+    {
+        $data = array(
+            'foo'   => 'bar',
+        );
+
+        $this->setRequestDeleteData( $data )->shouldReturn( NULL );
+    }
+
+    function it_has_a_setRequestPostData_method()
+    {
+        $data = array(
+            'foo'   => 'bar',
+        );
+
+        $this->setRequestPostData( $data )->shouldReturn( NULL );
+    }
+
+    function it_has_a_setMemoryLimit_method()
+    {
+        $memoryLimit = ( 2 * 1024 * 1024 );
+
+        $this->setMemoryLimit( $memoryLimit )->shouldReturn( NULL );
+    }
+
+    function it_has_a_setProxy_method()
+    {
+        $this->setProxy( '12.34.56.789' )->shouldReturn( NULL );
+        $this->getProxy()->shouldReturn( '12.34.56.789' );
+    }
+
+    function it_has_a_setProxyPort_method()
+    {
+        $this->setProxyPort( '8080' )->shouldReturn( NULL );
+        $this->getProxyPort()->shouldReturn( '8080' );
+    }
+
+    //Reset checks
+    function it_has_a_resetRequestHeaders_method()
+    {
+        $this->resetRequestHeaders()->shouldReturn( NULL );
+    }
+
+    function it_has_a_resetRequestCredentials_method()
+    {
+        $this->resetRequestCredentials()->shouldReturn( NULL );
+    }
+
+    function it_has_a_resetRequest_method()
+    {
+        $this->setRequestUri( 'localhost' );
+        $this->setRequestMethod( 'POST' );
+        $this->setRequestData( array() );
+        $this->setRequestCredentials( 'user', 'passwd' );
+        $this->setRequestHeader( 'test', 'value' );
+
+        $this->resetRequest()->shouldReturn( NULL );
+
+        $this->getRequestUri()->shouldReturn( FALSE );
+    }
+}
--- src/Infinity/Common/Http/README.md
+++ src/Infinity/Common/Http/README.md
@@ -40,3 +40,55 @@ $landHelper->run();
 var_dump($landHelper->isLand()); //Returns TRUE or FALSE
 
 ```
+
+HTTP Client
+=====================
+
+The HTTP Client provides an interface for setting up and processing HTTP requests
+
+Example usage:
+
+```php
+use Infinity\Common\Http\Client;
+
+$client = new Client();
+
+//Set the URI
+$client->setRequestUri( 'localhost' );
+
+//Set request data
+$client->setRequestData( array() );
+$client->setRequestMethod( 'POST' );
+
+//Set request data and method
+$client->setRequestPostData( array() ); //POST
+$client->setRequestPutData( array() ); //PUT
+$client->setRequestDeleteData( array() ); //DELETE
+
+//Sets request credentials
+$client->setRequestCredentials( 'user', 'passwd' );
+
+//Set request headers
+$client->setRequestheader( 'User-Agent', 'user-agent string' );
+
+//Advanced
+$client->setChecksMode( *bool* );
+$client->setFollowRedirects( *bool* );
+$client->setMemoryLimit( *int* ); //Memory limit in bytes
+$client->setProxy( *string* ); //Proxy address
+$client->setProxyPort( *string* ); //Proxy port
+
+//Send request
+$client->sendRequest();
+
+//Response
+var_dump($client->getResponseHeader( 'content-type' ));
+var_dump($client->getResponseCode());
+var_dump(stream_get_contents( $client->getResponseHandle() ));
+var_dump($client->getResponseSize());
+var_dump($client->getResponseResult());
+
+//Errors
+var_dump($client->getError());
+
+```
